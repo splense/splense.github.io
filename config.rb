fda7321b9bbf28afec47f0494a180463c389d0ab
Abhinav Jha <abhinavjha@outlook.com>
2015-04-15 18:43:54 +0530

    Add font awesome and replace nav buttons

--- config.rb
+++ config.rb
@@ -7,7 +7,6 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
-
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :compressed
 
